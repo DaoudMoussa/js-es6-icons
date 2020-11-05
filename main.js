e3d3099c779a845498d47ab02648cfbd38f5b837
Daoud <99m.daoud@gmail.com>
2020-11-05 16:43:32 +0100

    aggiunta prima e seconda milestone

---
+++ main.js
@@ -0,0 +1,141 @@
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template
+// literal, visualizzare in pagina tutte le icone con il
+// proprio nome.
+
+// definire un array di colori e associare ad ogni
+// tipo di icona un colore.
+// Visualizzare le icone di colore diverso in base al tipo.
+
+$(document).ready(() => {
+    const icons = [
+        {
+            name: 'cat',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'crow',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dog',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dove',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dragon',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'horse',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'hippo',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'fish',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'carrot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'apple-alt',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'lemon',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'pepper-hot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'user-astronaut',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-graduate',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-ninja',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-secret',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        }
+    ];
+
+    const colors = ['green', 'lightblue', 'brown']
+    const types = [];
+    icons.forEach(icon => {
+        const {type} = icon;
+
+        if(!types.includes(type)) {
+            types.push(type);
+        }
+    });
+
+    icons.forEach(icon => {
+        const {name, prefix, family, type} = icon;
+        const typeIndex = types.indexOf(type);
+        const iconColor = colors[typeIndex];
+
+        $('.container').append(`
+            <div class="icon-wrapper">
+                <i class="${family} ${prefix}${name}" style="color: ${iconColor}"></i>
+                <p class="name-icon">${name}</p>
+            </div>
+        `)
+
+
+    });
+
+
+
+
+
+})
